a13cd47e1a015ff74300fc7d12f3f88385f1a111
NAnhDuy <duynaFX11041@funix.edu.vn>
2021-11-12 14:27:56 +0700

    Revert "first commit"
    This reverts commit cba56bb69d4ddb4131c827cadd366db63c29dea7.

--- css/responsive.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("mymenue");
-    if (x.className === "menue") {
-        x.className += " responsive";
-    } else {
-        x.className = "menue";
-    }
-}
